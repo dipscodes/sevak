2abc63a6210b30ba9666056c46e4ff184819e516
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-03-09 18:14:57 +0530

    working sidebar square not icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,11 @@
+// import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+// import { FaPoo } from 'react-icons/fa';
+import SidebarIcon from './SidebarIcon';
+
 export default function Sidebar() {
   return (
     <div className="h-screen w-16 bg-discord-sidebar">
-      <span>something</span>
+      <SidebarIcon />
     </div>
   );
 }
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 // import ApiKey from '../components/ApiKey';
-import Sidebar from '../components/Sidebar';
+import Sidebar from 'components/Sidebar';
 
 export default function App() {
   return (
