4d4672004becef51e5bbd81f1912c8a743acd7d9
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-03-09 19:19:19 +0530

    icons are rendered

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,15 @@
+// import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
 export default function Sidebar() {
   return (
     <div className="h-screen w-16 bg-discord-sidebar">
-      <span>something</span>
+      <div className="sidebar-icon group">
+        <FaFire size={40} />
+      </div>
+      <div className="sidebar-icon group">
+        <FaPoo size={40} />
+      </div>
     </div>
   );
 }
