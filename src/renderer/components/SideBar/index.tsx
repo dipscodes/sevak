79b3c2675f3da4b2f8d4515efced3ce28c8edc29
rhitabratakarar <kararrhitabrata@gmail.com>
2023-03-16 19:01:15 +0530

    cleared cluttered code in sidebar.tsx and made a utils directory

--- src/renderer/components/SideBar/index.tsx
+++ src/renderer/components/SideBar/index.tsx
@@ -1,64 +1,24 @@
 import Page from 'renderer/components/Page';
 import SidebarIcon from 'renderer/components/SideBarIcon';
-import { useState } from 'react';
-// import fs from 'fs';
-import {
-  AiOutlinePoweroff,
-  AiOutlineKey,
-  AiOutlineApi,
-  AiOutlinePlusCircle,
-} from 'react-icons/ai';
-import { BsDroplet } from 'react-icons/bs';
-
-// function getAllPages() {
-//   const allPages = fs
-//     .readdirSync('components/Pages', {
-//       withFileTypes: true,
-//     })
-//     .filter((dirent: any) => dirent.isDirectory());
-//   return allPages;
-// }
+import { useState, useRef } from 'react';
+import getAllPages from 'renderer/utils/SideBar';
 
 export default function Sidebar() {
   // eslint-disable-next-line no-unused-vars
   const [pageView, setPageView] = useState('Token');
-  // const allPages = getAllPages();
-  // eslint-disable-next-line no-console
-  // console.log(allPages);
+  const icons = useRef(getAllPages());
 
   return (
     <div className="flex flex-row w-full">
       <div className="h-screen w-16 bg-discord-sidebar">
-        <SidebarIcon
-          icon=<AiOutlineApi size={30} />
-          onClick={() => {
-            setPageView('Token');
-          }}
-        />
-        <SidebarIcon
-          icon=<AiOutlinePoweroff size={30} />
-          onClick={() => {
-            setPageView('Power');
-          }}
-        />
-        <SidebarIcon
-          icon=<BsDroplet size={30} />
-          onClick={() => {
-            setPageView('Droplet');
-          }}
-        />
-        <SidebarIcon
-          icon=<AiOutlineKey size={30} />
-          onClick={() => {
-            setPageView('Key');
-          }}
-        />
-        <SidebarIcon
-          icon=<AiOutlinePlusCircle size={30} />
-          onClick={() => {
-            setPageView('Import');
-          }}
-        />
+        {Object.keys(icons.current).map((key: string) => (
+          <SidebarIcon
+            icon={icons.current[key]}
+            onClick={() => {
+              setPageView(key);
+            }}
+          />
+        ))}
       </div>
       <Page pageView={pageView} id="Token" />
       <Page pageView={pageView} id="Power" />
