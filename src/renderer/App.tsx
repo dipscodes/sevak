b750ef4c293d790e9b4a80d3ce2a5de7681ffba8
Deep Khamaru <deepkumarkhamaru@google.com>
2023-03-21 15:11:12 +0530

    Co-authored-by: Rhitabrata Karar <rhitabratakarar@users.noreply.github.com>

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Window from 'renderer/components/Window';
+import 'react-checkbox-tree/lib/react-checkbox-tree.css';
 
 export default function App() {
   return (
--- src/renderer/components/Pages/Export/index.tsx
+++ src/renderer/components/Pages/Export/index.tsx
@@ -1,5 +1,68 @@
+/* eslint-disable no-shadow */
+import { useEffect, useState } from 'react';
+import CheckboxTree from 'react-checkbox-tree';
+import { AiOutlineMinusSquare, AiOutlinePlusSquare } from 'react-icons/ai';
+import { BsChevronDown, BsChevronRight, BsDashSquare } from 'react-icons/bs';
+import { FaCheckSquare } from 'react-icons/fa';
+
+const nodes = [
+  {
+    value: 'DropletActions',
+    label: 'Droplet Actions',
+    children: [
+      { value: 'droplet_action-power_on', label: 'Power On' },
+      { value: 'droplet_action-power_off', label: 'Power Off' },
+      { value: 'droplet_action-reboot', label: 'Reboot' },
+    ],
+  },
+  {
+    value: 'mars2',
+    label: 'Mars2',
+    children: [
+      { value: 'phobos2', label: 'Phobos2' },
+      { value: 'deimos2', label: 'Deimos2' },
+    ],
+  },
+  {
+    value: 'mars3',
+    label: 'Mars3',
+    children: [
+      { value: 'phobos3', label: 'Phobos3' },
+      { value: 'deimos3', label: 'Deimos3' },
+    ],
+  },
+];
+
 export default function Export() {
+  const [checked, setChecked] = useState(['']);
+  const [expanded, setExpanded] = useState(['']);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(checked, expanded);
+  }, [checked, expanded]);
+
   return (
-    <div className="page-common text-text-generic-color">Export View.</div>
+    <div className="page-common text-text-generic-color justify-center">
+      <CheckboxTree
+        nodes={nodes}
+        checked={checked}
+        expanded={expanded}
+        icons={{
+          check: <FaCheckSquare size={20} />,
+          uncheck: <BsDashSquare size={20} />,
+          halfCheck: <BsDashSquare size={20} />,
+          expandClose: <BsChevronRight size={20} />,
+          expandOpen: <BsChevronDown size={20} />,
+          expandAll: <AiOutlinePlusSquare size={20} />,
+          collapseAll: <AiOutlineMinusSquare size={20} />,
+          parentClose: undefined,
+          parentOpen: undefined,
+          leaf: undefined,
+        }}
+        onCheck={(chk) => setChecked(chk)}
+        onExpand={(exp) => setExpanded(exp)}
+      />
+    </div>
   );
 }
