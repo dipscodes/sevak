ea9717e21b0c0dab2cca0ed7985544d9409af234
rhitabratakarar <kararrhitabrata@gmail.com>
2023-03-21 19:12:55 +0530

    fixed

--- src/main/utils/CheckedListToJson.ts
+++ src/main/utils/CheckedListToJson.ts
@@ -1,20 +1,24 @@
-// import { readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { PermissionObject } from './Interfaces';
 
 export default function checkedListToJson(
   checkedList: string[]
 ): PermissionObject {
-  const permissionObject = {
-    token: 'token',
-    droplet_action: {
-      power_on: false,
-      power_off: false,
-      reboot: false,
-    },
-  };
-  // const permissionTemplate: string = readFileSync('main/utils/template.json', {
-  //   encoding: 'utf-8',
-  // });
+  // const permissionObject = {
+  //   token: 'token',
+  //   droplet_action: {
+  //     power_on: false,
+  //     power_off: false,
+  //     reboot: false,
+  //   },
+  // };
+  const permissionTemplate: string = readFileSync(
+    `${__dirname}/template.json`,
+    {
+      encoding: 'utf-8',
+    }
+  );
+  const permissionObject = JSON.parse(permissionTemplate);
   if (!checkedList) {
     return permissionObject;
   }
