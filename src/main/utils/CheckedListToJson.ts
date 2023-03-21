ea366dac0962a2a0fe4067b5f8c9c6bc38648fb6
rhitabratakarar <kararrhitabrata@gmail.com>
2023-03-21 19:02:49 +0530

    fixed wrong file content issue while exporting

--- src/main/utils/CheckedListToJson.ts
+++ src/main/utils/CheckedListToJson.ts
@@ -1,18 +1,17 @@
 // import { readFileSync } from 'fs';
 import { PermissionObject } from './Interfaces';
 
-const permissionObject = {
-  token: 'token',
-  droplet_action: {
-    power_on: false,
-    power_off: false,
-    reboot: false,
-  },
-};
-
 export default function checkedListToJson(
   checkedList: string[]
 ): PermissionObject {
+  const permissionObject = {
+    token: 'token',
+    droplet_action: {
+      power_on: false,
+      power_off: false,
+      reboot: false,
+    },
+  };
   // const permissionTemplate: string = readFileSync('main/utils/template.json', {
   //   encoding: 'utf-8',
   // });
