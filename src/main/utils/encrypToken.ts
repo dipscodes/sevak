80c302e245bd9f7f4b658cb5c18b8f3eba466ca9
Deep Khamaru <deepkumarkhamaru@google.com>
2023-03-22 15:12:41 +0530

    encryption works in exported file

--- src/main/utils/encrypToken.ts
+++ src/main/utils/encrypToken.ts
@@ -1,11 +1,8 @@
 import * as crypto from 'crypto';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 async function encrypt(permission: string): Promise<Array<string>> {
   const algorithm: string = 'aes-256-cbc';
-  const initVector = process.env.INIT_VECTOR as string;
+  const initVector: string = 'bd5fac96dd725e297f87bf255e3aadb0';
   const securitykey: Buffer = crypto.randomBytes(32);
 
   const cipher = crypto.createCipheriv(
@@ -15,8 +12,6 @@ async function encrypt(permission: string): Promise<Array<string>> {
   );
   let encryptedData = cipher.update(permission, 'utf-8', 'hex');
   encryptedData += cipher.final('hex');
-  // eslint-disable-next-line no-console
-  // console.log([securitykey.toString('hex'), encryptedData]);\
   const passKey = securitykey.toString('hex');
 
   return [passKey, encryptedData];
