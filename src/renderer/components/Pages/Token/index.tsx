f18a93d63496ba0ed06609ef28b80362f8e2be43
Deep Khamaru <deepkumarkhamaru@google.com>
2023-03-23 12:23:15 +0530

    Co-authored-by: Rhitabrata Karar <kararrhitabrata@gmail.com>

--- src/renderer/components/Pages/Token/index.tsx
+++ src/renderer/components/Pages/Token/index.tsx
@@ -4,15 +4,28 @@ export default function Token() {
   const [file, setFile] = useState('Select token file');
   return (
     <div className="page-common text-text-generic-color">
-      <button
-        type="button"
-        onClick={async () => {
-          const filePath = await window.electron.openFile();
-          setFile(filePath);
-        }}
-      >
-        {file}
-      </button>
+      <div className="flex flex-row justify-center pt-5 pb-5 bg-discord-bg-1 shadow-lg">
+        <button
+          className="discord-button mr-2"
+          type="button"
+          onClick={async () => {
+            const filePath = await window.electron.openFile();
+            setFile(filePath);
+          }}
+        >
+          {file}
+        </button>
+        <button
+          className="discord-button ml-2"
+          type="button"
+          onClick={async () => {
+            const filePath = await window.electron.openFile();
+            setFile(filePath);
+          }}
+        >
+          {file}
+        </button>
+      </div>
     </div>
   );
 }
