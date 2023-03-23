97402a02821291d96b1e8f65991cf11510c95e48
Deep Khamaru <deepkumarkhamaru@google.com>
2023-03-23 15:10:00 +0530

    large input field css class added

--- src/renderer/components/ApiModal/index.tsx
+++ src/renderer/components/ApiModal/index.tsx
@@ -53,7 +53,7 @@ export default function ApiModal({
           <input
             type="password"
             id="large-input"
-            className="placeholder-gray-600 block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="discord-large-input"
             placeholder="Token Key"
           />
         </div>
@@ -61,7 +61,7 @@ export default function ApiModal({
           <input
             type="text"
             id="large-input"
-            className="placeholder-gray-600 block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="discord-large-input"
             placeholder="Name"
           />
         </div>
