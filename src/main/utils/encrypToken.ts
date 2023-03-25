c4a662bafd868ffd7eb2df74368be96b3bfc169b
Deep Khamaru <deepkumarkhamaru@google.com>
2023-03-25 16:35:49 +0530

    encrptToken unit tested

--- src/main/utils/encrypToken.ts
+++ src/main/utils/encrypToken.ts
@@ -1,20 +1,32 @@
 import * as crypto from 'crypto';
 
-async function encrypt(permission: string): Promise<Array<string>> {
+function inflate(targetLength: number, password?: string): Buffer | undefined {
+  if (password === undefined) {
+    return password;
+  }
+  const multiplier = Math.ceil(targetLength / password.length);
+  return Buffer.from(password.repeat(multiplier).substring(0, 32), 'utf-8');
+}
+
+async function encrypt(
+  genericString: string,
+  password?: string
+): Promise<Array<string>> {
   const algorithm: string = 'aes-256-cbc';
   const initVector: string = 'bd5fac96dd725e297f87bf255e3aadb0';
   const securitykey: Buffer = crypto.randomBytes(32);
-
+  const inflatedPassword = inflate(32, password);
   const cipher = crypto.createCipheriv(
     algorithm,
-    Buffer.from(securitykey),
+    Buffer.from(inflatedPassword ?? securitykey),
     Buffer.from(initVector, 'hex')
   );
-  let encryptedData = cipher.update(permission, 'utf-8', 'hex');
+
+  let encryptedData = cipher.update(genericString, 'utf-8', 'hex');
   encryptedData += cipher.final('hex');
   const passKey = securitykey.toString('hex');
 
-  return [passKey, encryptedData];
+  return [!password ? passKey : '', encryptedData];
 }
 
 export default encrypt;
