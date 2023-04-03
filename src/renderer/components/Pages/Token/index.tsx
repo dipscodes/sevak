bf0bb69b274e8bfe720813b83d9052af73ec5469
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-04-03 17:03:55 +0530

    list ui done

--- src/renderer/components/Pages/Token/index.tsx
+++ src/renderer/components/Pages/Token/index.tsx
@@ -39,7 +39,7 @@ export default function Token() {
   }
 
   return (
-    <div className="page-common text-text-generic-color">
+    <div className="page-common">
       <div className="flex flex-row justify-center pt-5 pb-5 bg-discord-bg-1 shadow-lg">
         <button
           className="discord-button mr-2"
--- src/renderer/components/RowOfToken/index.tsx
+++ src/renderer/components/RowOfToken/index.tsx
@@ -1,12 +1,23 @@
+import { BsChevronRight } from 'react-icons/bs';
+
 interface Props {
   name: string;
 }
 
 export default function RowOfToken({ name }: Props) {
   return (
-    <div>
-      <span>{name}</span>
-      <button type="button">Delete</button>
+    <div className="w-80 h-10 flex flex-row justify-between py-2 px-4 my-1">
+      <div className="flex flex-row text-text-generic-color">
+        <BsChevronRight className="my-auto" size={18} />
+        <span className="pl-2 font-semibold">{name}</span>
+      </div>
+
+      <button
+        className="rounded-full px-2 bg-red-500 text-white shadow-lg hover:shadow-none font-light"
+        type="button"
+      >
+        Delete
+      </button>
     </div>
   );
 }
