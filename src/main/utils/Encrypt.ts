a00e57575253725d696b4e2bcef98852bd5bd03f
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-04-06 13:38:30 +0530

    Encrypt Class done Co-authored-by: Rhitabrata Karar <kararrhitabrata@gmail.com>

---
+++ src/main/utils/Encrypt.ts
@@ -0,0 +1,56 @@
+import * as crypto from 'crypto';
+
+export default class Encrypt {
+  /**
+   * Takes input of permission string ({token: 'something', 'droplet_actions': {...}})
+   * Encrypts the data and sends back(returns) a security key.
+   * @param permissionString: normal string with no encryption applied.
+   */
+  static encryptPermissionString(permissionString: string) {
+    const algorithm: string = 'aes-256-cbc';
+    const initVector: string = 'bd5fac96dd725e297f87bf255e3aadb0';
+    const securitykey: Buffer = crypto.randomBytes(32);
+    const cipher = crypto.createCipheriv(
+      algorithm,
+      Buffer.from(securitykey),
+      Buffer.from(initVector, 'hex')
+    );
+
+    let encryptedPermissionString = cipher.update(
+      permissionString,
+      'utf-8',
+      'hex'
+    );
+    encryptedPermissionString += cipher.final('hex');
+    const passKey = securitykey.toString('hex');
+
+    return [passKey, encryptedPermissionString];
+  }
+
+  /**
+   * static function that takes normal password (decrypted string) as input and encrypts with
+   * master password (decrypted string).
+   * @param normalPassword: normal string (decrypted)
+   * @param masterPassword: normal string (decrypted)
+   */
+  static encryptNormalPassword(normalPassword: string, masterPassword: string) {
+    const algorithm: string = 'aes-256-cbc';
+    const initVector: string = 'bd5fac96dd725e297f87bf255e3aadb0';
+    const inflatedPassword: string = this.#inflate(32, masterPassword);
+    const cipher = crypto.createCipheriv(
+      algorithm,
+      Buffer.from(inflatedPassword),
+      Buffer.from(initVector, 'hex')
+    );
+
+    let encryptedPassword = cipher.update(normalPassword, 'utf-8', 'hex');
+    encryptedPassword += cipher.final('hex');
+
+    return encryptedPassword;
+  }
+
+  static #inflate(targetLength: number, password: string): string {
+    const multiplier = Math.ceil(targetLength / password.length);
+    return password.repeat(multiplier).substring(0, 32);
+  }
+}
