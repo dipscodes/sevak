57c5acad9ef8149ca93cbc010a3033c3a4fa176c
Deep Kumar Khamaru <deepkumarkhamaru@gmail.com>
2023-04-30 13:54:57 +0530

    buttons decorated and visual aid added

--- src/renderer/components/CardComponent/index.tsx
+++ src/renderer/components/CardComponent/index.tsx
@@ -1,3 +1,7 @@
+import { AiOutlinePoweroff } from 'react-icons/ai';
+import { GrRotateRight } from 'react-icons/gr';
+import { VscOpenPreview } from 'react-icons/vsc';
+
 export default function CardComponent() {
   return (
     <div className="mb-5 mt-5">
@@ -11,10 +15,18 @@ export default function CardComponent() {
           </div>
         </div>
         <div className="buttonDiv">
-          <div className="action-button power-on" />
-          <div className="action-button power-off" />
-          <div className="action-button reboot" />
-          <div className="action-button view" />
+          <div className="action-button power-on action-button-common">
+            <AiOutlinePoweroff size={30} />
+          </div>
+          <div className="action-button power-off action-button-common">
+            <AiOutlinePoweroff className="rotate-180" size={30} />
+          </div>
+          <div className="action-button reboot action-button-common">
+            <GrRotateRight size={30} />
+          </div>
+          <div className="action-button view action-button-common">
+            <VscOpenPreview size={30} />
+          </div>
         </div>
       </div>
     </div>
